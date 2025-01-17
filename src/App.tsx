2c3d1878e8b18cd3db549295711e6083909204c4
jonghoon <jonghoon7431@gmail.com>
2025-01-17 15:06:02 +0900

    setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+function App() {
+  return (
+    <>
+      <div className="w-9 bg-slate-400">
+        Click on the Vite and React logos to learn more
+      </div>
+    </>
+  );
+}
+
+export default App;
