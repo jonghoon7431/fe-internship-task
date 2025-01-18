196933c6c3f0274b49599054037f318be503646c
jonghoon <jonghoon7431@gmail.com>
2025-01-18 21:51:14 +0900

    feat: 회원가입 페이지 유효성 검사 적용 및 회원가입 요청 로직 작성
    회원가입 요청에서 timeout  에러 발생, 서버 작동 여부 확인해야할 것 같음

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,9 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { signUp } from "../api/auth";
+import {
+  isIdValidate,
+  isNicknameValidate,
+  isPasswordValidate,
+} from "../utils/validate";
 
 const SignUp = () => {
-  const handleSubmit = (e: React.FormEvent) => {
+  const [nickName, setNickName] = useState("");
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [validationErrors, setValidationErrors] = useState<{
+    [key: string]: boolean;
+  }>({});
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (name === "nickName") {
+      setValidationErrors((prev) => ({
+        ...prev,
+        [name]: !isNicknameValidate(value),
+      }));
+    }
+    if (name === "id") {
+      setValidationErrors((prev) => ({
+        ...prev,
+        [name]: !isIdValidate(value),
+      }));
+    }
+    if (name === "password") {
+      setValidationErrors((prev) => ({
+        ...prev,
+        [name]: !isPasswordValidate(value),
+      }));
+    }
+    if (name === "passwordCheck") {
+      setPasswordCheck(value);
+      setValidationErrors((prev) => ({
+        ...prev,
+        passwordCheck: password !== value,
+      }));
+    }
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    console.log("work");
     e.preventDefault();
+
+    const response = await signUp(id, password, nickName);
+    console.log(response);
   };
 
   return (
@@ -16,21 +63,43 @@ const SignUp = () => {
           <input
             id="nickName"
             type="text"
-            placeholder="한글 혹은 영어로 입력해주세요"
+            placeholder="특수문자 제외 10글자 이내로 입력해주세요"
             className="input-auth"
             autoComplete="off"
+            minLength={1}
+            maxLength={10}
+            value={nickName}
+            name="nickName"
+            onChange={(e) => setNickName(e.target.value)}
+            onInput={handleInput}
           />
+          {validationErrors.nickName && (
+            <p className="text-red-600 text-xs">
+              특수문자 제외 1글자 이상 입력해주세요
+            </p>
+          )}
         </div>
 
         <div className="flex flex-col">
-          <label htmlFor="userName">아이디</label>
+          <label htmlFor="id">아이디</label>
           <input
-            id="userName"
+            id="id"
             type="text"
             placeholder="3글자 이상으로 입력해주세요"
             className="input-auth"
             autoComplete="off"
+            minLength={3}
+            maxLength={10}
+            name="id"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+            onInput={handleInput}
           />
+          {validationErrors.id && (
+            <p className="text-red-600 text-xs">
+              영어와 숫자를 포함해 3글자 이상으로 입력해주세요
+            </p>
+          )}
         </div>
 
         <div className="flex flex-col">
@@ -38,10 +107,21 @@ const SignUp = () => {
           <input
             id="password"
             type="password"
-            placeholder="영문, 숫자 혼합 8자 이상으로 입력해주세요"
+            placeholder="영문, 숫자 혼합 6자 이상으로 입력해주세요"
             className="input-auth"
             autoComplete="new-password"
+            minLength={6}
+            maxLength={16}
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            onInput={handleInput}
           />
+          {validationErrors.password && (
+            <p className="text-red-600 text-xs">
+              영문, 숫자 혼합 6자 이상으로 입력해주세요
+            </p>
+          )}
         </div>
 
         <div className="flex flex-col">
@@ -52,7 +132,16 @@ const SignUp = () => {
             placeholder="비밀번호를 다시 입력해주세요"
             className="input-auth"
             autoComplete="new-password"
+            minLength={6}
+            maxLength={16}
+            name="passwordCheck"
+            value={passwordCheck}
+            onChange={(e) => setPasswordCheck(e.target.value)}
+            onInput={handleInput}
           />
+          {validationErrors.passwordCheck && (
+            <p className="text-red-600 text-xs">비밀번호가 일치하지 않습니다</p>
+          )}
         </div>
 
         <div className="flex flex-col gap-3 mt-4">
