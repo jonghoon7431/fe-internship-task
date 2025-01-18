2e25cedaf545ecc5075a0ae9ecb1e27401de3d3a
jonghoon <jonghoon7431@gmail.com>
2025-01-18 21:50:07 +0900

    style: ui 변경

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,10 +2,9 @@ import React from "react";
 import { Link, Outlet } from "react-router-dom";
 
 const Layout = () => {
-  //TODO 홈버튼
   return (
     <div className="w-full h-screen p-6 bg-slate-600">
-      <div className="w-[40%] min-w-[600px] h-[78%] bg-white mx-auto mt-[10%] rounded-xl p-3">
+      <div className="w-[40%] min-w-[600px]  min-h-[74%] bg-white mx-auto mt-[10%] rounded-xl px-3 py-[1rem]">
         <Link to={"/"}>
           <button className="text-xl font-semibold">HOME</button>
         </Link>
