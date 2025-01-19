8af02d74a74df2abb0c4ba8e578d31aad71011fc
jonghoon <jonghoon7431@gmail.com>
2025-01-19 17:35:00 +0900

    style: layout 비율 수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ import { Link, Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <div className="w-full h-screen p-6 bg-slate-600">
-      <div className="w-[40%] min-w-[600px]  min-h-[74%] bg-white mx-auto mt-[10%] rounded-xl px-3 py-[2rem]">
+      <div className="w-[36%] min-w-[600px]  min-h-[74%] bg-white mx-auto mt-[6%] rounded-xl px-3 py-[2rem]">
         <Link to={"/"}>
           <button className="text-xl font-semibold py-2 absolute">HOME</button>
         </Link>
